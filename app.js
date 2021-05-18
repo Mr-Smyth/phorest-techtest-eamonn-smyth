a1113cf656a13db6dc89410b917d5ac6b746eb9f
eamonn <e.smyth75@gmail.com>
2021-05-18 18:39:25 +0100

    Add Static files into public folder
    *	Create a CSS file in the public/main folder
    *	Add some custom CSS
    *	Copy new styling to all templates

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 // ====== REQUIRES ======
+const path = require('path');
 const express = require('express');
+
 
 // ------ REGISTER ROUTES ------
 
 const app = express();
 const mainRoutes = require('./routes/main');
+const errorController = require('./controllers/error');
+
+// TELL EXPRESS TO LOOK IN PUBLIC FOLDER FOR LOCAL ASSETS
+app.use(express.static(path.join(__dirname, 'public')));
 
 // SETUP OUR EJS TEMPLATES
 app.set('view engine', 'ejs');
 
 // OUTSOURCED ROUTES
 app.use('/', mainRoutes);
+app.use(errorController.get404);
 
 app.listen(3000);
---
+++ utils/path.js
@@ -0,0 +1,6 @@
+
+//  ====== HELPER - GETS THE ROOT DIRECTORY ======
+
+const path = require('path');
+
+module.exports = path.dirname(require.main.filename);
