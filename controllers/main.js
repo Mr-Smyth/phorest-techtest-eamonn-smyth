8a125813dece35ce59d9867eb11cf6868721aaeb
eamonn <e.smyth75@gmail.com>
2021-05-18 21:36:25 +0100

    Add initial search form layout to search page

--- controllers/main.js
+++ controllers/main.js
@@ -7,28 +7,28 @@
 exports.getIndex = (req, res, next) => {
     res.render('index', {
         pageTitle: 'Home page',
-        path :'index',
+        path :'/index',
     });
 };
 
 exports.getSearch = (req, res, next) => {
     res.render('search', {
         pageTitle: 'Client Search',
-        path :'search',
+        path :'/search',
     });
 };
 
 exports.getVoucher = (req, res, next) => {
     res.render('voucher', {
         pageTitle: 'Voucher Create',
-        path :'index',
+        path :'/voucher',
     });
 };
 
 exports.getAbout = (req, res, next) => {
     res.render('about', {
         pageTitle: 'About page',
-        path :'about',
+        path :'/about',
     });
 };
 
